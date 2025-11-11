6bd1c5126b7a7a7897dc356dbc6770cbc274ff93
x3nl1 <stepanagapov2005@gmail.com>
2025-11-12 02:31:46 +0300

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -1,8 +1,8 @@
 def multiplication_table():
-    size = int(input("Введите размер таблицы умножения: "))
     for i in range(1, size + 1):
         for j in range(1, size + 1):
             print(i * j, end="\t")
         print()
-
+        
+size = int(input("Введите размер таблицы умножения: "))
 multiplication_table()
