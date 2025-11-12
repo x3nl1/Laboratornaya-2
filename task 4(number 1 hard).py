4ac714e48b3c22550b6cbac2c662c72a2617b4a2
x3nl1 <stepanagapov2005@gmail.com>
2025-11-12 09:08:32 +0300

    Update task 4(number 1 hard).py

--- task 4(number 1 hard).py
+++ task 4(number 1 hard).py
@@ -1,6 +1,7 @@
 def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
